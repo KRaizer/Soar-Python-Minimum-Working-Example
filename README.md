268174b5f9f528f76d6439f3fac0a9e57b508de6
Klaus <klaus.raizer@ericsson.com>
2018-01-30 16:36:12 -0200

    finished first version of toy-env example

--- README.md
+++ README.md
@@ -36,4 +36,51 @@ You should see the following:
 
 	Hello world
 	1:    ==>S: S2 (state no-change)  
+
+## Run toy-env.py for an example of using IO
+
+Run the toy environmnet example:
+
+	python toy-env.py
+ 
+You should see something like this:
+
+	(' ------------- Soar cycle: ', 0, ' ------------- ')
+	('---> Environment sensed: ', [7, 9])
+	===> Soar output: 16.000000
+	
+	===> Soar input: 7.000000 9.000000
+	1:    O: O2 (initialize-toy-env)
+	('---> Environment acted:', 0)
+	(' ------------- Soar cycle: ', 1, ' ------------- ')
+	('---> Environment sensed: ', [7, 3])
+	===> Soar output: 10.000000
+	
+	===> Soar input: 7.000000 3.000000
+	2:    ==>S: S2 (state no-change)
+	('---> Environment acted:', 16.0)
+	(' ------------- Soar cycle: ', 2, ' ------------- ')
+	('---> Environment sensed: ', [3, 4])
+	===> Soar output: 7.000000
+	
+	===> Soar input: 3.000000 4.000000
+	3:    ==>S: S3 (state no-change)
+	('---> Environment acted:', 10.0)
+	soar> 
+
+The goal of this exercise is just to show how to use the IO interface and perform a simple cognitive cycle inside a Soar agent.
+The "toy environment" is just a function that produces two random numbers and receives a single result.
+This agent receives these numbers through "sensors", add them up, and return them through actuators.
+
+# Learn more at
+
+1) Soar Manual http://soar.eecs.umich.edu/downloads/Documentation/SoarManual.pdf
+
+2) Soar Tutorials
+
+3) Python Interface Example https://soar.eecs.umich.edu/articles/downloads/examples-and-unsupported/183-python-interface-example
+
+4) Python SML Interface file /Soar/build/Core/ClientSMLSWIG/Python/Python_sml_ClientInterface.py
+
+
   
--- toy-env.py
+++ toy-env.py
@@ -1,12 +1,14 @@
-"""Making sure we are running right version of python"""
+"""Making sure we are running the right version of python"""
 import sys
 if sys.version_info[0] >= 3:
     raise "Must be using Python 2"
 
-"""Making sure soar library path environment is set"""
+"""Making sure soar library path environment is set
+Remember to set the environment variable to point to where soar build is located, e.g.:
+export LD_LIBRARY_PATH=~/Desktop/Soar/out
+"""
 from os import environ as env, fsync
 import sys
-
 if "DYLD_LIBRARY_PATH" in env:
 	LIB_PATH = env["DYLD_LIBRARY_PATH"]
 elif "LD_LIBRARY_PATH" in env:
@@ -31,16 +33,11 @@ def cli(agent):
 		if cmd:
 			print(agent.ExecuteCommandLine(cmd).strip())
 		cmd = raw_input("soar> ")
-#""" Output monitor"""
-#def output_event_handler(obj, agent_name, attribute, wme):
-#    if wme.IsJustAdded(): #attribute == "result" and 
-#        res = float(wme.GetValueAsString())#(int)(wme.GetValueAsString())
-#        print("output::::::::: ",res)
 
 from random import *
 class ToyEnv(object):
     """
-    A very simple 'environment': sensors retur two random numbers and expects a single number as actuation.
+    A very simple 'environment': sensors return two random numbers and expects a single number as actuation.
     """
     def __init__(self):
         """Return a new toy env object."""
@@ -65,48 +62,39 @@ if __name__ == "__main__":
     #Instantiate link to environment
     te = ToyEnv()
 
-
     #Create soar kernel and agent
     kernel = sml.Kernel.CreateKernelInCurrentThread()
     agent = kernel.CreateAgent("agent")
     register_print_callback(kernel, agent, callback_print_message, None)
 
-
-#    #Create a monitor for outputs
-#    agent.AddOutputHandler("test", output_event_handler, None)
-
     #Load soar sources
     agent.ExecuteCommandLine("source toy-env.soar")
-#    print(agent.ExecuteCommandLine("source wj.soar"))
 
-    ##Get input link and create input  structure
+    #Get input link and create input  structure
     input_link=agent.GetInputLink()
     
-
     a_value=agent.CreateFloatWME(input_link, "a", -1.0)
     b_value=agent.CreateFloatWME(input_link, "b", -1.0)
 
     #Get output link
     output_link=agent.GetOutputLink()
 
-
     ### Start Soar cognitive cycle ###
     #
-    for i in range(0,3):
+    for i in range(0,3): # replace by a "while True:" to run forever
         print(" ------------- Soar cycle: ",i," ------------- ")
     # 1) sense the environment
         sense=te.get_sensors()
 
-        # 2) push senses to soar
+    # 2) push senses to soar
         a_value.Update(sense[0])
         b_value.Update(sense[1])
 
-
-        # 3) make soar think about it
+    # 3) make soar think about it
         result=0
-        run_result=agent.RunSelf(2)    #Run agent for one step
+        run_result=agent.RunSelf(1)    #Run agent for one step
     
-        # 4) get results from soar
+    # 4) get results from soar
         output_link=agent.GetOutputLink()## returns an Identifier
         if output_link!= None:
             result_output_wme = output_link.FindByAttribute("result", 0) # returns a WMElement of the form (<output_link> ^result <val>)
@@ -114,14 +102,13 @@ if __name__ == "__main__":
             if result_output_wme != None:
                 result = float(result_output_wme.GetValueAsString())
 
-
-        #send result to environment
+    #5) send result to environment
         te.set_actuators(result) 
-
-    #cli(agent)
 
     #
     ### End Soar cognitive cycle###
+
+    cli(agent) #open client to interact with agent
 
     #Close agent and kernel
     kernel.DestroyAgent(agent)
